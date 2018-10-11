2bcdeff52fe44df27663cd0822138077c4486710
martynasssz <martynasssk@gmail.com>
2018-10-11 14:33:40 +0300

    js paskutinin pataisymai

---
+++ javascript_add.js
@@ -0,0 +1,12 @@
+function didinti() {
+	console.log('Showing');
+	jonas.style.display = 'inline';
+}
+
+function mazinti() {
+	console.log('Hiding');
+	jonas.style.display = 'none';
+	jonas.styl
+
+	
+}
